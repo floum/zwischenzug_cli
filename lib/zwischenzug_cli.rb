f306bd15c94bab3ff7132697e91670d77608855e
Efflam Castel <efflamm.castel@gmail.com>
2020-02-14 03:45:56 +0100

    loading puzzles to the api, at last

--- lib/zwischenzug_cli.rb
+++ lib/zwischenzug_cli.rb
@@ -1,7 +1,11 @@
-require "zwischenzug_cli/version"
+require 'pgn'
+require 'json'
+require 'net/http'
+require 'zwischenzug_cli/version'
 require 'zwischenzug_cli/puzzle'
 require 'zwischenzug_cli/analysis'
 require 'zwischenzug_cli/lichess'
+require 'zwischenzug_cli/zwischenzug_api'
 
 module ZwischenzugCLI
   class Error < StandardError; end
---
+++ lib/zwischenzug_cli/zwischenzug_api.rb
@@ -0,0 +1,43 @@
+module ZwischenzugCLI
+  class ZwischenzugAPI
+    def initialize(options = {})
+      @username = options[:username]
+      @password = options[:password]
+      @host = options[:host]
+      @port = options[:port]
+    end
+
+    def create(puzzle)
+      game = PGN.parse(puzzle['pgn']).first
+
+      request_body = {
+        challenges: puzzle['challenges'].map do |challenge|
+          if challenge[-1] == 'b'
+            black_ply = 1
+          else
+            black_ply = 0
+          end
+          ply_count = challenge[0..1].to_i * 2 + black_ply - 2
+
+          {
+            fen: game.positions[ply_count].to_fen,
+            expected_fens: [game.positions[ply_count+1].to_fen]
+          }
+        end
+      }
+
+      response = Net::HTTP.start(puzzle_uri.host, puzzle_uri.port) do |http|
+        request = Net::HTTP::Post.new puzzle_uri, 'Content-Type' => 'application/json'
+        request.body = request_body.to_json
+
+        http.request request
+      end
+
+      p response
+    end
+
+    def puzzle_uri
+      URI.parse "http://#{@host}:#{@port}/puzzles"
+    end
+  end
+end
